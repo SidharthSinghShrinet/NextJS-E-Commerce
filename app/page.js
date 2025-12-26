b8f7f83b9f8b5bfc3204331c153152d8bd582b1e
SidharthSinghShrinet <tushar15463@gmail.com>
2025-12-26 17:39:30 +0530

    Redux is integrated and in it and got tested also

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
-import React from "react";
-
 function Page() {
-  return <div>Welcome to the Home Page...</div>;
+  return (
+    <div>
+      <h1>Welcome to the Home Page...</h1>
+    </div>
+  );
 }
 
 export default Page;
